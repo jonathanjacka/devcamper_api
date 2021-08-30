3b0e6f6a8d779c401f7366f8e1ee12106f5578c5
Jonathan Jacka <jonathanjacka@gmail.com>
2021-08-29 21:56:29 -0500

    Added basic routes for bootcamps routes

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+//Route files
+const bootcampRoutes = require('./routes/bootcamps');
+
 //Load env variables
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+//Mount routers
+app.use('/api/v1/bootcamps/', bootcampRoutes);
 
 const PORT = process.env.PORT || 5000;
 
