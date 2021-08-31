43e20cfcfc4d9fae78a80836a75f59d30646c142
Jonathan Jacka <jonathanjacka@gmail.com>
2021-08-31 06:02:49 -0500

    Added logger middleware with morgan

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
 //Route files
 const bootcampRoutes = require('./routes/bootcamps');
-
 //Load env variables
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+//dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 //Mount routers
 app.use('/api/v1/bootcamps/', bootcampRoutes);
