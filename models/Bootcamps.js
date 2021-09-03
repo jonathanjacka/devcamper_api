d88f4eb35e6d112f62668e4bbd4f0e0e92f784a8
Jonathan Jacka <jonathanjacka@gmail.com>
2021-09-03 06:16:22 -0500

    Added _data file and begin to contruct controllers - incomplete!

---
+++ models/Bootcamps.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const BootcampSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a name'],
+    unique: true,
+    trim: true,
+    maxlength: [50, 'Name must be 50 characters or fewer'],
+  },
+  slug: String,
+});
