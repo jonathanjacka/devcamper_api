4a6a5e2157e2b66f8ff281a8cdb8d5df18e82c56
Jonathan Jacka <jonathanjacka@gmail.com>
2021-09-11 07:23:14 -0500

    Updated routes/courses.js

--- routes/courses.js
+++ routes/courses.js
@@ -4,6 +4,5 @@ const router = express.Router({ mergeParams: true });
 const { getCourses } = require('../controllers/courses');
 
 router.route('/').get(getCourses);
-//router.route('/bootcamps/:bootcampId/courses').get(getCourses);
 
 module.exports = router;
