3d210fbd7c854d0e076593679c7d8596c1dc642f
Jonathan Jacka <jonathanjacka@gmail.com>
2021-09-12 06:13:05 -0500

    Updated GET single course with courses route and controller

--- controllers/courses.js
+++ controllers/courses.js
@@ -26,3 +26,27 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
     data: courses,
   });
 });
+
+//@desc     Get a single course
+//@route    GET /api/v1/courses/:id
+//@access   Public
+exports.getCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id).populate({
+    path: 'bootcamp',
+    select: 'name description',
+  });
+
+  if (!course) {
+    return next(
+      new ErrorResponse(
+        `No course with the id of ${req.params.id} was found - this means that the course doesn't exist or that the id you entered is incorrect`,
+        404
+      )
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
