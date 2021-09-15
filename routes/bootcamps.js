6e9695d18a49333c7e389937e42f5b3cd7581407
Jonathan Jacka <jonathanjacka@gmail.com>
2021-09-14 21:51:57 -0500

    Refactored filtering functionings into advancedResults middleware

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -11,6 +11,10 @@ const {
   bootcampPhotoUpload,
 } = require('../controllers/bootcamps');
 
+const Bootcamp = require('../models/Bootcamp');
+
+const advancedResults = require('../middleware/advancedResults');
+
 //include other resource routers
 const coursesRoutes = require('./courses');
 
@@ -19,7 +23,10 @@ router.use('/:bootcampId/courses', coursesRoutes);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 router.route('/:id/photo').put(bootcampPhotoUpload);
-router.route('/').get(getBootcamps).post(createBootcamp);
+router
+  .route('/')
+  .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
+  .post(createBootcamp);
 
 router
   .route('/:id')
