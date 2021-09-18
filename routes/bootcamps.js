87915c6a9345558a15b669176e166103d963295e
Jonathan Jacka <jonathanjacka@gmail.com>
2021-09-18 06:04:00 -0500

    Added role authorization to POST, PUT, and DELETE requests

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -14,7 +14,7 @@ const {
 const Bootcamp = require('../models/Bootcamp');
 
 const advancedResults = require('../middleware/advancedResults');
-const { protect } = require('../middleware/auth');
+const { protect, authorize } = require('../middleware/auth');
 
 //include other resource routers
 const coursesRoutes = require('./courses');
@@ -23,16 +23,18 @@ const coursesRoutes = require('./courses');
 router.use('/:bootcampId/courses', coursesRoutes);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
-router.route('/:id/photo').put(protect, bootcampPhotoUpload);
+router
+  .route('/:id/photo')
+  .put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload);
 router
   .route('/')
   .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
-  .post(protect, createBootcamp);
+  .post(protect, authorize('publisher', 'admin'), createBootcamp);
 
 router
   .route('/:id')
   .get(getBootcamp)
-  .put(protect, updateBootcamp)
-  .delete(protect, deleteBootcamp);
+  .put(protect, authorize('publisher', 'admin'), updateBootcamp)
+  .delete(protect, authorize('publisher', 'admin'), deleteBootcamp);
 
 module.exports = router;
