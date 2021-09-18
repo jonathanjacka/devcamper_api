d7f0421a000a0d9fd72f7dd2e3d84019ad9c7a30
Jonathan Jacka <jonathanjacka@gmail.com>
2021-09-17 22:10:29 -0500

    Added routes and controllers via auth for user registration, log in, and GET log in with token

--- routes/courses.js
+++ routes/courses.js
@@ -11,6 +11,7 @@ const {
 
 const Course = require('../models/Course');
 const advancedResults = require('../middleware/advancedResults');
+const { protect } = require('../middleware/auth');
 
 router
   .route('/')
@@ -21,7 +22,11 @@ router
     }),
     getCourses
   )
-  .post(addCourse);
-router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
+  .post(protect, addCourse);
+router
+  .route('/:id')
+  .get(getCourse)
+  .put(protect, updateCourse)
+  .delete(protect, deleteCourse);
 
 module.exports = router;
