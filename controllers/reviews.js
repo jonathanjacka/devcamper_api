2a112f4d856f455597f080f659d2ce826c10ad64
Jonathan Jacka <jonathanjacka@gmail.com>
2021-09-24 06:19:24 -0500

    Added Review update and delete - issue with recalc of averageRating and averageCost

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -68,3 +68,74 @@ exports.addReview = asyncHandler(async (req, res, next) => {
     data: review,
   });
 });
+
+//@desc     Update review
+//@route    PUT /api/v1/reviews/:id
+//@access   Private
+exports.updateReview = asyncHandler(async (req, res, next) => {
+  let review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(
+      new ErrorResponse(
+        `No review with the id of ${req.params.id} was found.`,
+        404
+      )
+    );
+  }
+
+  //make sure review belongs to user/user is 'admin'
+  if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to update review ${req.params.id}`,
+        401
+      )
+    );
+  }
+
+  review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  await review.save();
+
+  res.status(200).json({
+    success: true,
+    data: review,
+  });
+});
+
+//@desc     Delete review
+//@route    Delete /api/v1/reviews/:id
+//@access   Private
+exports.deleteReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(
+      new ErrorResponse(
+        `No review with the id of ${req.params.id} was found.`,
+        404
+      )
+    );
+  }
+
+  //make sure review belongs to user/user is 'admin'
+  if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to delete review ${req.params.id}`,
+        401
+      )
+    );
+  }
+
+  await review.remove();
+
+  res.status(200).json({
+    success: true,
+    data: review,
+  });
+});
