e8f3d697fa23c9e10186a648b5a975f82ee8ba03
Jonathan Jacka <jonathanjacka@gmail.com>
2021-09-25 06:30:29 -0500

    Added log out user route - see auth.js for options

--- controllers/auth.js
+++ controllers/auth.js
@@ -68,6 +68,18 @@ exports.getMe = asyncHandler(async (req, res, next) => {
   res.status(200).json({ success: true, data: req.user });
 });
 
+//@desc     Log user out/clear current cookie
+//@route    GET /api/v1/auth/logout
+//@access   Private
+exports.logOut = asyncHandler(async (req, res, next) => {
+  res.cookie('token', 'none', {
+    expires: new Date(Date.now() + 10000),
+    httpOnly: true,
+  });
+
+  res.status(200).json({ success: true, data: {} });
+});
+
 //@desc     Update user details
 //@route    GET /api/v1/auth/updatedetails
 //@access   Private
